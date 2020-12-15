9d4746439d9510f30b13d42304aa8ac8064adaf0
Ree5 <reesalumasa@gmail.com>
2020-12-15 13:24:09 +0300

    Update ci/cd deployment to hostinger

---
+++ storage/framework/views/0511e22e55c736612fe6b7c0cab308b8.php
@@ -0,0 +1,8 @@
+
+<p></p>
+<?php echo controller()->renderComponent('slider'); ?>
+<p></p>
+<?php echo controller()->renderComponent('localSearch'); ?>
+<p></p>
+<?php echo controller()->renderComponent('featuredItems'); ?>
+<p></p>
