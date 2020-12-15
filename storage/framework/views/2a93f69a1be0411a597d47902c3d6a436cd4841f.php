1811bad5bbc78815f9a368a23effb36ffd3897f0
Ree5 <reesalumasa@gmail.com>
2020-12-15 13:53:34 +0300

    Update ci/cd deployment to hostinger

--- storage/framework/views/2a93f69a1be0411a597d47902c3d6a436cd4841f.php
+++
@@ -1,3 +0,0 @@
-<?php echo $this->makeFormFieldWidget($field)->render(); ?>
-
-<?php /**PATH C:\xampp\htdocs\laravel\tasty\setup-master/app/admin/widgets/form/field_widget.blade.php ENDPATH**/ ?>
--- storage/framework/views/e28963bb4daa5e1cf7089b780422be086ba24432.php
+++
@@ -1,6 +0,0 @@
-<?php echo $this->makePartial($item->path ?: $item->itemName, [
-    'menuItem' => $item,
-    'item' => $item,
-]); ?>
-
-<?php /**PATH C:\xampp\htdocs\laravel\tasty\setup-master/app/admin/widgets/menu/item_partial.blade.php ENDPATH**/ ?>
